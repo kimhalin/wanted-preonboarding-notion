c2a3f0c8dc2e47f5c37ce208acc48052881f500d
jongseong <eello.bdev@gmail.com>
2023-09-04 23:29:56 +0900

    refactor: 리팩토링 및 수정
    - breadcrumbs를 페이지 ID -> title로 수정
    - jdbc -> JPA native query 사용

--- src/main/java/com/example/wantedpreonboarding/PageService.java
+++ src/main/java/com/example/wantedpreonboarding/PageService.java
@@ -1,38 +1,26 @@
 package com.example.wantedpreonboarding;
 
-
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.stereotype.Service;
-
-import java.sql.ResultSet;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class PageService {
-    private final DriverManagerDataSource dataSource =
-            new DriverManagerDataSource("jdbc:mysql://localhost:13306/notion", "root", "root");
-    private final JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+
+    private final PageRepository pageRepository;
 
     public PageDto getPage(Long id) {
-        String breadCrumbsSql = "SELECT parent_page_id FROM sub_pages WHERE sub_page_id = ? ORDER BY depth ASC";
-        String subPageSql = "SELECT sub_page_id FROM sub_pages WHERE parent_page_id = ? AND depth = 1";
-        String getPageSql = "SELECT * FROM pages p WHERE id = ?";
-        List<Long> breadCrumbs = jdbcTemplate.query(breadCrumbsSql, (ResultSet rs, int rowNum) -> {
-            return rs.getLong("parent_page_id");
-        }, id);
-        List<Long> subpages = jdbcTemplate.query(subPageSql, (ResultSet rs, int rowNum) -> {
-            return rs.getLong("sub_page_id");
-        }, id);
-        Page page = jdbcTemplate.queryForObject(getPageSql, (ResultSet rs, int rowNum) -> {
-            return new Page(rs.getLong("id"), rs.getString("title"), rs.getString("content"));
-        }, id);
+        Page page = pageRepository.findByPageId(id)
+            .orElseThrow(() -> new EntityNotFoundException("page not found."));
 
-        return PageDto.builder()
-                .id(page.getId())
-                .title(page.getTitle())
-                .content(page.getContent())
-                .breadCrumbs(breadCrumbs)
-                .subPages(subpages).build();
+        List<Long> subPages = pageRepository.findSubPagesByPageId(id);
+
+        List<String> breadCrumbs = pageRepository.findBreadCrumbsByPageId(id);
+
+        return PageDto.of(page, subPages, breadCrumbs);
     }
 }
---
+++ src/test/java/com/example/wantedpreonboarding/PageRepositoryTest.java
@@ -0,0 +1,22 @@
+package com.example.wantedpreonboarding;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PageRepositoryTest {
+
+	@Autowired
+	private PageRepository repository;
+
+	@Test
+	public void test() {
+
+	}
+
+}
